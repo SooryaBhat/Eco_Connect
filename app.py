919c587858599b9b0ea3cc73ab335694adc3ce95
SooryaBhat <sooryabhat49@gmail.com>
2025-11-04 19:03:44 +0000

    modify in app.py

--- app.py
+++ app.py
@@ -459,4 +459,5 @@ def government_analytics():
     return render_template('government/analytics.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
