f983b85c3314f4a8addf640bc58e5ad198c09b3e
SooryaBhat <sooryabhat49@gmail.com>
2025-11-10 02:31:03 +0000

    add replit.nix

---
+++ replit.nix
@@ -0,0 +1,15 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.sqlalchemy
+    pkgs.python311Packages.flask-login
+    pkgs.python311Packages.werkzeug
+    pkgs.python311Packages.pillow
+    pkgs.python311Packages.pymysql
+    pkgs.python311Packages.gunicorn
+  ];
+}
