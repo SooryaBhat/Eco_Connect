dcb15bd018a4ea19dc041104b1a0d4a965d1e0bc
SooryaBhat <sooryabhat49@gmail.com>
2025-11-11 16:09:47 +0000

    some instalments from requirements.txt

--- replit.nix
+++ replit.nix
@@ -5,11 +5,12 @@
     pkgs.python311Packages.setuptools
     pkgs.python311Packages.wheel
     pkgs.python311Packages.flask
-    pkgs.python311Packages.sqlalchemy
     pkgs.python311Packages.flask-login
+    pkgs.python311Packages.sqlalchemy
     pkgs.python311Packages.werkzeug
     pkgs.python311Packages.pillow
     pkgs.python311Packages.pymysql
     pkgs.python311Packages.gunicorn
   ];
 }
+
