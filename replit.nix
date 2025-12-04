0b930716b9ba25c21f2125fe0e4bd9811c447dc2
SooryaBhat <sooryabhat49@gmail.com>
2025-12-04 02:36:58 +0000

    changes in pyproject

--- replit.nix
+++ replit.nix
@@ -1,20 +1,20 @@
 { pkgs }: {
   deps = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.setuptools
-    pkgs.python311Packages.wheel
+    pkgs.python310
+    pkgs.python310Packages.pip
+    pkgs.python310Packages.setuptools
+    pkgs.python310Packages.wheel
 
-    # Flask + Extensions
-    pkgs.python311Packages.flask
-    pkgs.python311Packages.flask_login
-    pkgs.python311Packages.sqlalchemy
-    pkgs.python311Packages.werkzeug
-    pkgs.python311Packages.pillow
-    pkgs.python311Packages.pymysql
-    pkgs.python311Packages.gunicorn
+    pkgs.python310Packages.flask
+    pkgs.python310Packages.flask_login
+    pkgs.python310Packages.jinja2
+    pkgs.python310Packages.babel
+    pkgs.python310Packages.flask_babel
 
-    # Babel (for translations)
-    pkgs.python311Packages.babel
+    pkgs.python310Packages.sqlalchemy
+    pkgs.python310Packages.pymysql
+    pkgs.python310Packages.pillow
+    pkgs.python310Packages.werkzeug
+    pkgs.python310Packages.gunicorn
   ];
 }
